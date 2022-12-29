259ce185d30f267b0b288c285f5bd3cc428387c1
Md_Bosir <80116157+bosirullah@users.noreply.github.com>
2022-12-29 20:12:08 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![screenshot.jpeg](./screenshot.jpeg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Mobile-first workflow
+- javascript
+- [bootstrap](https://getbootstrap.com/) - React framework
+
+### What I learned
+
+In this section, I learnt how to apply events using javascript for html and css
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@bosirullah](https://www.frontendmentor.io/profile/bosirullah)
---
+++ index.js
@@ -0,0 +1,102 @@
+var count = 1;
+var circle = document.querySelector(".circle");
+circle.style.marginLeft = "6px";
+
+document.querySelector(".switch").addEventListener("click", function (){
+    if(count === 0 || count >= 3){
+        circle.style.marginLeft = "6px";
+        count = 0;
+    }
+    else{
+        var style = circle.style.marginLeft;
+        var result;
+        if(style[1] === "p"){
+            result = (style[0] - "0");
+        }
+        else result = (style[0] - "0") * 10 + (style[1] - "0")
+
+        result += 22;
+        circle.style.marginLeft = result + "px";  
+    }
+    count++;
+    count %= 4;
+    switchTheme2(count);
+})
+
+function switchTheme2(count){
+    var link = document.querySelectorAll("link");
+    link[3].setAttribute("href","styles/style" + count +".css")
+}
+
+var length = document.querySelectorAll(".keys").length;
+var res = "";
+var len;
+var num1 = "",num2 = 0;
+var op = "";
+for(var i = 0;i<=length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var key = this.textContent;
+        if(key === "1" || key === "2" || key === "3" || key === "4" || key === "5" || key === "6" || key === "7" || key === "8" || key === "9" || key === "0" || key === "."){
+            res += key;
+            document.querySelector(".screen p").textContent = res;
+        }
+
+        if(res !== "" && (key === "+" || key === "-" || key === "x" || key === "/")){
+            num1 = res;
+            reset();
+            num1 -= "0";
+            num2 += num1;
+        }
+
+        switch(key){
+            case "DEL":
+                document.querySelector(".screen p").textContent = deleteNum(res);
+                res = deleteNum(res);
+            break;
+
+            case "RESET":
+                reset();
+            break;
+
+            case "+":
+                op = "+";
+            break;
+
+            case "-":
+                op = "-"
+            break;
+
+            case "x":
+                op = "x"
+            break;
+
+            case "/":
+                op = "/"
+            break;
+
+            case "=":
+                res -= "0";
+                var ans;
+                if(op === "+") ans = num2 + res;
+                else if(op === "-") ans = num2 - res;
+                else if(op === "x") ans = num2 * res;
+                else if(op === "/") ans = num2 / res;
+                document.querySelector(".screen p").textContent = ans + "";
+            break;
+
+            default: console.log(key);
+        }
+    })
+}
+
+function deleteNum(res){
+    return res.slice(0,-1);
+}
+
+function reset(){
+    document.querySelector(".screen p").textContent = "";
+    res = "";
+    num2 = 0;
+}
+
+
